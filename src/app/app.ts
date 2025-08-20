d639be80f182bb830c2d42cf146dda4162f152b4
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-20 21:30:40 +0300

    feat(services): create and use svg icon service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+import { SvgIcons } from './services/svg-icons/svg-icons';
 
 @Component({
 	selector: 'app-root',
-	imports: [],
+	imports: [MatIconModule],
 	templateUrl: './app.html',
 	styleUrl: './app.css'
 })
 export class App {
-	protected title = 'frontend';
+	public constructor(svgIcons: SvgIcons) {
+		svgIcons.registerIcons({
+			change: 'change.svg',
+			currency: 'currency.svg',
+			email: 'email.svg',
+			logout: 'logout.svg',
+			market: 'market.svg',
+			markets: 'markets.svg',
+			nav: 'nav.svg',
+			phone: 'phone.svg',
+			projects: 'projects.svg',
+			telegram: 'telegram.svg',
+			user: 'user.svg'
+		});
+	}
 }
---
+++ src/app/services/svg-icons/svg-icons.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class SvgIcons {
+	public constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {}
+
+	public registerIcons(sources: { [name: string]: string }) {
+		for (const name in sources) {
+			this.matIconRegistry.addSvgIcon(name, this.domSanitizer.bypassSecurityTrustResourceUrl('/images/' + sources[name]!));
+		}
+	}
+}
