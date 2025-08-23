6762822c50fc66f3af142df276755c854976192b
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-23 16:05:30 +0300

    feat(ui): add border for table row, add isSpecial flag for table cell and add onChange for table cell item

--- src/app/ui/widgets/table-cell/table-cell.ts
+++ src/app/ui/widgets/table-cell/table-cell.ts
@@ -14,4 +14,20 @@ import { BaseTableItem } from '../base-table-item/base-table-item';
 })
 export class TableCell extends BaseTableItem<string, TableCellItem> {
 	@Input({ required: true }) public items: TableCellConfig<string> = {};
+
+	protected onNumberValueChange(item: { number: number; onChange?: (value: number) => void }, value: number) {
+		item.number = value;
+
+		if (item.onChange !== undefined) {
+			item.onChange(value);
+		}
+	}
+
+	protected onTextValueChange(item: { text: string; onChange?: (value: string) => void }, value: string) {
+		item.text = value;
+
+		if (item.onChange !== undefined) {
+			item.onChange(value);
+		}
+	}
 }
--- src/app/ui/widgets/table-cell/table-cell.types.ts
+++ src/app/ui/widgets/table-cell/table-cell.types.ts
@@ -4,9 +4,9 @@ import { ITextInput } from '../../controls/text-input/text-input.types';
 export type TableCellItem =
 	| { type: 'icon'; iconSrc: string }
 	| { type: 'number'; number: number }
-	| { type: 'number'; number: number; config: Partial<INumberInput>; isEditing?: true }
-	| { type: 'text'; text: string }
-	| { type: 'text'; text: string; config: Partial<ITextInput>; isEditing?: true }
+	| { type: 'number'; number: number; config: Partial<INumberInput>; onChange?: (value: number) => void; isEditing?: true }
+	| { type: 'text'; text: string; isSpecial?: true }
+	| { type: 'text'; text: string; config: Partial<ITextInput>; onChange?: (value: string) => void; isEditing?: true }
 	| { type: 'button'; text: string };
 
 export type TableCellConfig<T extends string | symbol | number> = Record<T, TableCellItem>;
