fe0151407b7f6ffed7b011a54da03d8b570e9bf7
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-23 05:52:53 +0300

    refactor(ui): rename EntityCreate to EntityNameForm component

--- src/app/ui/shared/entity-name-form/entity-name-form.ts
+++ src/app/ui/shared/entity-name-form/entity-name-form.ts
@@ -1,14 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+
 import { TextInput } from '../../controls/text-input/text-input';
 import { ButtonInput } from '../../controls/button-input/button-input';
 
 @Component({
 	selector: 'app-entity-create',
 	imports: [TextInput, ButtonInput],
-	templateUrl: './entity-create.html',
-	styleUrl: './entity-create.css'
+	templateUrl: './entity-name-form.html',
+	styleUrl: './entity-name-form.css'
 })
-export class EntityCreate {
+export class EntityNameForm {
 	@Input({ required: true }) public submitText: string = '';
 
 	@Output() public submited = new EventEmitter<string>();
