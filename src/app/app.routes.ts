5fa5efd9eeb3a62c4f5c8286939fbdc24cf9e452
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-24 12:36:59 +0300

    feat(features): create project component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 	{ path: '', loadComponent: () => import('./features/home/home').then((c) => c.Home) },
 	{ path: 'auth', loadComponent: () => import('./features/auth/auth').then((c) => c.Auth) },
-	{ path: 'projects', loadComponent: () => import('./features/projects/projects/projects').then((c) => c.Projects) }
+	{ path: 'projects', loadComponent: () => import('./features/projects/projects/projects').then((c) => c.Projects) },
+	{ path: 'project/:id', loadComponent: () => import('./features/projects/project/project').then((c) => c.Project) }
 ];
