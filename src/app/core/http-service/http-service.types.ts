40157f40800f30d9272d5d96d665ee7ac189fddb
PasinZhuga01 <sergijumanskij@gmail.com>
2025-09-16 20:41:01 +0300

    feat(core): remove unused HttpService type QueryParams

--- src/app/core/http-service/http-service.types.ts
+++ src/app/core/http-service/http-service.types.ts
@@ -1,2 +1,1 @@
-export type QueryParams = Record<string, string | number | boolean | ReadonlyArray<string | number | boolean>>;
 export type SuccessResult<T extends object> = { success: true; response: T } | { success: false; response: { code: unknown } };
