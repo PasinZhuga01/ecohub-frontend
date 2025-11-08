ae25ec23e9a187ee7161c3091fb795aa41fd56a0
PasinZhuga01 <sergijumanskij@gmail.com>
2025-11-08 20:28:15 +0200

    feat(ui): improve Sidebar and SidebarItem
    - use fields instead of config-object and ConfigManager in Sidebar, SidebarItem
    - use ng-content instead of items field in Sidebar, SidebarItem
    - remove clicked event from Sidebar
    - use color for children instead of level in SidebarItem
    - move icon to text container in SidebarItem

--- src/app/ui/layout/sidebar-item/sidebar-item.ts
+++ src/app/ui/layout/sidebar-item/sidebar-item.ts
@@ -1,9 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
-import { ConfigManager } from '@core/managers';
-
-import { SidebarItemInputConfig, SidebarItemConfig, SidebarItemExpansion, SidebarItemClickEvent } from './sidebar-item.types';
 
 @Component({
 	selector: 'app-sidebar-item',
@@ -11,35 +8,12 @@ import { SidebarItemInputConfig, SidebarItemConfig, SidebarItemExpansion, Sideba
 	templateUrl: './sidebar-item.html',
 	styleUrl: './sidebar-item.css'
 })
-export class SidebarItem<T> {
-	public readonly config = input<SidebarItemInputConfig<T>>({ id: -1, text: '' });
-	public readonly clicked = output<SidebarItemClickEvent<T>>();
+export class SidebarItem {
+	public readonly value = input.required<string>();
 
-	protected readonly _configManager = new ConfigManager<SidebarItemConfig<T>>(
-		{
-			id: -1,
-			text: '',
-			isSpecial: false,
-			level: 0
-		},
-		this.config
-	);
+	public readonly isSpecial = input<boolean>(false);
+	public readonly iconName = input<string>();
+	public readonly isExpanded = model<boolean>();
 
-	protected _onClick(config: SidebarItemConfig<T>) {
-		if ('value' in config) {
-			return this.clicked.emit({ value: config.value, isExplicit: true });
-		}
-
-		this.clicked.emit({ value: config, isExplicit: false });
-	}
-
-	protected _toggleExpanded(expansion: SidebarItemExpansion<T>) {
-		this._configManager.set({ expansion: { ...expansion, isExpanded: !expansion.isExpanded } });
-	}
-
-	protected _prepareItemConfig(config: SidebarItemInputConfig<T>, nextLevel: number): SidebarItemInputConfig<T> {
-		config.level = nextLevel;
-
-		return config;
-	}
+	public readonly clicked = output<void>();
 }
--- src/app/ui/layout/sidebar/sidebar.ts
+++ src/app/ui/layout/sidebar/sidebar.ts
@@ -1,18 +1,12 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SidebarItem } from '../sidebar-item/sidebar-item';
-import { SidebarItemClickEvent, SidebarItemInputConfig } from '../sidebar-item/sidebar-item.types';
 
 @Component({
 	selector: 'app-sidebar',
-	imports: [SidebarItem, CommonModule],
+	imports: [CommonModule],
 	templateUrl: './sidebar.html',
 	styleUrl: './sidebar.css'
 })
-export class Sidebar<T> {
-	public readonly items = input.required<SidebarItemInputConfig<T>[]>();
+export class Sidebar {
 	public readonly CSSAbsolutePositionConfig = input<Partial<Record<'left' | 'right' | 'top' | 'bottom', string>> | null>(null);
-
-	public readonly clicked = output<SidebarItemClickEvent<T>>();
 }
