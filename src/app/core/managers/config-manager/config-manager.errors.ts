414e1c1e4d87c46563fd0ff7fbc98325332b8b83
PasinZhuga01 <sergijumanskij@gmail.com>
2025-11-08 20:35:15 +0200

    feat(core): remove ConfigManager

--- src/app/core/managers/config-manager/config-manager.errors.ts
+++
@@ -1,7 +0,0 @@
-import { BaseError } from 'ecohub-shared/errors';
-
-export class ConfigManagerError extends BaseError {
-	public constructor(message: string) {
-		super('ConfigManagerError', message);
-	}
-}
--- src/app/core/types/index.ts
+++ src/app/core/types/index.ts
@@ -1,14 +1,3 @@
-import { z } from 'zod';
-
-/* config */
-
-export type ConfigSchema<TConfig extends object> = {
-	validators?: Partial<{ [K in keyof TConfig]: z.ZodSchema<TConfig[K]> }>;
-	normalize?: (config: TConfig) => TConfig;
-};
-
-/* utils */
-
 export type PartialExcept<T, K extends keyof T> = Partial<T> & Pick<T, K>;
 export type SuccessableObject<TSuccess extends object = object, TError extends object = object> =
 	| ({ success: true } & TSuccess)
