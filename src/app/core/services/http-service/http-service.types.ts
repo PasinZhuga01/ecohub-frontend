7d6bb457f55730cbf7cfb83494f5a44d33defae8
PasinZhuga01 <sergijumanskij@gmail.com>
2025-11-13 16:32:40 +0200

    refactor(core): use new types from shared lib for HttpService

--- src/app/core/services/http-service/http-service.types.ts
+++ src/app/core/services/http-service/http-service.types.ts
@@ -1,4 +1,4 @@
 import { SuccessableObject } from '@core/types';
-import { codes } from 'ecohub-shared/http/payloads';
+import { Code } from 'ecohub-shared/http/payloads';
 
-export type HttpResult<T extends object> = SuccessableObject<{ response: T }, { payload: { code: keyof typeof codes } }>;
+export type HttpResult<T extends object> = SuccessableObject<{ response: T }, { payload: { code: Code } }>;
